36ba7977ef41f94039a402194dfe0f2141f9b6a2
Muhammad Maulana Hikam <maulanahikam085@gmail.com>
2022-12-13 02:50:08 +0700

    Final Program v1.0

--- main.cpp
+++ main.cpp
@@ -9,10 +9,10 @@ int main(){
         cout << " =      Program Matriks      =" << endl;
         cout << " =============================" << endl;
         cout << " = 1. Buat Matriks           =" << endl;
-        cout << " = 2. Penjumlahan Matriks    =" << endl;
-        cout << " = 3. Perkalian Matriks      =" << endl;
-        cout << " = 4. Tranpose Matriks       =" << endl;
-        cout << " = 5. Determinan Matriks     =" << endl;
+        cout << " = 2. Perkalian Matriks      =" << endl;
+        cout << " = 3. Tranpose Matriks       =" << endl;
+        cout << " = 4. Determinan Matriks     =" << endl;
+        cout << " = 5. Invers Matriks         =" << endl;
         cout << " = 6. Tampil Matriks         =" << endl;
         cout << " = 7. Keluar                 =" << endl;
         cout << " =============================" << endl;
@@ -23,38 +23,52 @@ int main(){
             case 1 : system("cls");{
                 matriks_1();
                 if (terisi == true){
-                    showMatriks();
+                    cout << "\nMatriks 1 : \n";
+                    showMatriks(matriks1);
                 }
                 getch();
                 break;
             }
             case 2 : system("cls");{
-                sumMatriks();
-                getch();
-                break;
-            }
-            case 3 : system("cls");{
                 multiplyMatriks();
                 getch();
                 break;
             }
-            case 4 : system("cls");{
+            case 3 : system("cls");{
                 tranposeMatriks();
+                getch();
+                break;
+            }
+            case 4 : system("cls");{
+                if (baris_1 != kolom_1){
+                    cout << "Maaf, matriks tidak bisa dicari determinan-nya!\n";
+                } else {
+                    sizeArr = baris_1;
+                    int det = determinantMatriks(matriks1, sizeArr);
+                    cout << "Determinan Matriks yaitu : " << det << "\n";
+                }
                 getch();
                 break;
             }
             case 5 : system("cls");{
                 if (baris_1 != kolom_1){
-                    cout << "Maaf, matriks tidak bisa dicari determinan-nya!\n";
+                    cout << "Maaf, matriks tidak bisa dicari inverse-nya!\n";
                 } else {
-                    size = baris_1;
-                    determinanMatriks();
+                    sizeArr = baris_1;
+                    if (inverse(matriks1, matriksInvers) == true){
+                        showMatriksInvers(matriksInvers);
+                    }
                 }
                 getch();
                 break;
             }
             case 6 : system("cls");{
-                showMatriks();
+                if (terisi == true){
+                    showMatriks(matriks1);
+                } else {
+                    cout << "Maaf belum ada matriks untuk ditampilkan!\n";
+                }
+                
                 getch();
                 break;
             }
--- pustaka.h
+++ pustaka.h
@@ -11,9 +11,11 @@
 using namespace std;
 
 //INISIALISASI BARIS DAN KOLOM
-int baris_1, kolom_1, baris_2, kolom_2, size;
+int baris_1, kolom_1, baris_2, kolom_2, sizeArr;
 int matriks1[100][100];
 int matriks2[100][100];
+int matriksAdjoint[100][100];
+float matriksInvers[100][100];
 
 //KONFIRMASI MATRIKS KOSONG
 bool terisi = false;
@@ -32,36 +34,6 @@ void matriks_1(){
     cout << "\nMatriks 1 berhasil dibuat!\n";
 }
 
-//FUNGSI PENJUMLAHAN MATRIKS
-void sumMatriks(){
-    int sum[100][100];
-    cout << "Masukkan jumlah baris matriks 1 : ";cin >> baris_2;
-    cout << "Masukkan jumlah kolom matriks 1 : ";cin >> kolom_2;
-    if (baris_1 != baris_2 || kolom_1 != kolom_2){
-        cout << "Maaf, kedua matriks tidak dapat dijumlahkan!";
-    } else {
-        for (int i = 0; i < baris_2; i++){
-            for (int j = 0; j < kolom_2; j++){
-                cout << "Masukkan data matriks2 baris-" << i+1 << " kolom-" << j+1 << " : ";
-                cin >> matriks2[i][j];
-            }
-        }
-        for (int i = 0; i < baris_1; i++){
-            for (int j = 0; j < kolom_1; j++){
-                sum[i][j] = matriks1[i][j] + matriks2[i][j];
-            }
-        }
-        cout << "\nHasil Penjumlahan Matriks : \n";
-        for (int i = 0; i < baris_1; i++){
-            cout << "[ ";
-            for (int j = 0; j < kolom_1; j++){
-                cout << sum[i][j] << " ";
-            }
-            cout << "]\n";
-        }
-    }
-}
-
 //FUNGSI PERKALIAN MATRIKS
 void multiplyMatriks(){
     cout << "Masukkan jumlah baris matriks 2 : ";cin >> baris_2;
@@ -75,6 +47,16 @@ void multiplyMatriks(){
                 cin >> matriks2[i][j];
             }
         }
+
+        cout << "\nMatriks 2 : \n";
+        for (int i = 0; i < baris_2; i++){
+            cout << "[ ";
+            for (int j = 0; j < kolom_2; j++){
+                cout << matriks2[i][j] << " ";
+            }
+            cout << "]\n";
+        }
+        getch();
         
         int mulMatriks[baris_1][kolom_2];
         for (int i = 0; i < baris_1; i++){
@@ -97,50 +79,86 @@ void multiplyMatriks(){
     }
 }
 
-//FUNGSI DETERMINAN MATRIKS
-void determinanMatriks(){
-    int index, data1, data2;
+//FUNGSI KOFAKTOR MATRIKS
+void cofactorMatriks(int mat[100][100], int temp[100][100], int a, int b, int sizeArr){
+    int i = 0;
+    int j = 0;
+
+    for (int baris = 0; baris < sizeArr; baris++){
+        for (int kolom = 0; kolom < sizeArr; kolom++){
+            if (baris != a && kolom != b){
+                temp[i][j++] = mat[baris][kolom];
+
+                if (j == sizeArr - 1){
+                    j = 0;
+                    i++;
+                }
+            }
+        }
+    }
+}
+
+// FUNGSI DETERMINAN MATRIKS
+int determinantMatriks(int det[100][100], int sizeArr){
     int D = 0;
-    int total = 1;
-    int det = 1;
-    int temp[size + 1];
 
-    for (int i = 0; i < size; i++){
-        index = i;
-
-        while (index < size && matriks1[index][i] == 0){
-            index++;
-        }
-        if (index == size){
-            continue;
-        }
-        if (index != i){
-            for (int j = 0; j < size; j++){
-                swap(matriks1[index][j], matriks1[i][j]);
-            }
-            det = det * pow(-1, index - i);
-        }
-
-        for (int j = 0; j < size; j++){
-            temp[j] = matriks1[i][j];
-        }
-        
-        for (int j = i + 1; j < size; j++){
-            data1 = temp[i];
-            data2 = matriks1[j][i];
-            for (int k = 0; k < size; k++){
-                matriks1[j][k] = (data1 * matriks1[j][k]) - (data2 * temp[k]);
-            }
-            total = total * data1;
-        }    
+    if (sizeArr == 1){
+        return det[0][0];
     }
 
-    for (int i = 0; i < size; i++){
-        det = det * matriks1[i][i];
+    int temp[100][100];
+
+    int sign = 1;
+    
+    for (int i = 0; i < sizeArr; i++){
+        cofactorMatriks(det, temp, 0, i, sizeArr);
+        D += sign * det[0][i] * determinantMatriks(temp, sizeArr - 1);
+
+        sign = -sign;
     }
     
-    D = (det/total);
-    cout << "Determinan Matriks yaitu " << D;
+    return D;
+}
+
+// FUNGSI MATRIKS ADJOIN
+void adjoinMatriks(int mat[100][100], int adj[100][100]){
+    if (sizeArr == 1){
+        adj[0][0] = 1;
+        return;
+    }
+    
+    int sign = 1;
+    int temp[100][100];
+
+    for (int i = 0; i < sizeArr; i++){
+        for (int j = 0; j < sizeArr; j++){
+            cofactorMatriks(mat, temp, i, j, sizeArr);
+
+            sign = ((i + j) % 2 == 0) ? 1 : -1;
+
+            adj[j][i] = (sign) * (determinantMatriks(temp, sizeArr - 1));
+        }
+    }
+}
+
+// FUNGSI INVERS MATRIKS
+bool inverse(int mat[100][100], float inv[100][100]){
+    int det = determinantMatriks(mat, sizeArr);
+    if(det == 0){
+        cout << "Matriks bersifat singular, jadi tidak dapat dicari inverse-nya";
+        return false;
+    }
+
+    int adj[100][100];
+    adjoinMatriks(mat, adj);
+
+    for (int i = 0; i < sizeArr; i++){
+        for (int j = 0; j < sizeArr; j++){
+            inv[i][j] = adj[i][j] / float(det);
+        }
+    }
+
+    return true;
 }
 
 //FUNGSI TRANPOSE MATRIKS
@@ -194,13 +212,24 @@ void tranposeMatriks(){
 }
 
 //FUNGSI UNTUK MENAMPILKAN MATRIKS
-void showMatriks(){
-    cout << "\nMatriks 1 : \n";
+void showMatriks(int mat[100][100]){
     for (int i = 0; i < baris_1; i++){
         cout << "[ ";
         for (int j = 0; j < kolom_1; j++){
-            cout << matriks1[i][j] << " ";
+            cout << mat[i][j] << " ";
         }
         cout << "]\n";
     }
+}
+
+// FUNGSI MENAMPILKAN MATRIKS INVERS
+void showMatriksInvers(float inv[100][100]){
+    for (int i = 0; i < sizeArr; i++){
+        cout << "[ ";
+        for (int j = 0; j < sizeArr; j++){
+            cout << inv[i][j] << " ";
+        }
+        cout << "]\n";
+    }
+    
 }
